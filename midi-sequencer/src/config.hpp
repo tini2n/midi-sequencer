16a99dcc78580a9e7cc5ca3ed2595673c042a0a5
tini2n <tini2n@gmail.com>
2025-09-05 23:41:26 +0300

    remove unnecesary

--- midi-sequencer/src/config.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <Arduino.h>
-
-namespace cfg {
-    constexpr uint32_t TICK_HZ = 1000; // Tick frequency in Hz
-    constexpr uint32_t TICK_US = 1000000UL / TICK_HZ; // Maximum microseconds per tick
-    constexpr size_t RB_CAP = 1024; // Ring buffer capacity
-}
